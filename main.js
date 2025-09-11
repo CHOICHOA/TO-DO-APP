1ddd39ec7d1a1b43c93e297161796e1ec3500919
CHOICHOA <choa_jj@naver.com>
2025-09-12 02:37:09 +0900

    Update main.js

--- main.js
+++ main.js
@@ -14,8 +14,21 @@ addButton.addEventListener("click", addTask);
 function addTask(){
   let taskContent = taskInput.value
   taskList.push(taskContent)
+  render();
 }
 
 function render(){
-  let resultHTML
+  let resultHTML = '';
+  for(let i = 0; i<taskList.length; i++) {
+  resultHTML += `<div class="task">
+    <div>${taskList[i]}</div>
+          <div>
+            <button>check</button>
+            <button>delete</button>
+          </div>
+    </div>`;
+  }
+     
+  
+  document.getElementById("task-board").innerHTML = resultHTML;
 }
